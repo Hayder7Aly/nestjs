65c46b5f7392e914d3fd5096201afa34911c8dd5
Hayder Aly <007harryjutt@gmail.com>
2023-08-13 23:25:25 +0500

    add .env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm'; // Import TypeOrmModule
 import { BookModule } from './book/book.module'; // Import the BookModule
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+
+
+const database = process.env.DATABASE_URL
+
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: "postgres://maujqhwm:yvgjg8wn6qFVWvQdeEbLDiHEcGC8Fz5O@trumpet.db.elephantsql.com/maujqhwm",
+      url: database,
       // host: 'trumpet.db.elephantsql.com',
       // port: 5432,
       // username: 'maujqhwm',
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
